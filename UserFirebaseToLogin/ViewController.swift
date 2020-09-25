a25a9bc81142b90bf4abe1cc32077af4d75a4c2b
UserFirebaseToLogin <hippo5406920@icloud.com>
2020-09-25 18:46:34 +0800

    UserFirebaseToLogin

---
+++ UserFirebaseToLogin/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  UserFirebaseToLogin
+//
+//  Created by CHEN on 2020/9/24.
+//
+
+import UIKit
+// import Firebase
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func Login(_ sender: UIButton) {
+    }
+    
+    @IBAction func registered(_ sender: UIButton) {
+    }
+}
+
